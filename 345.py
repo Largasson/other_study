2a3177a97d94b9d0ecc5d03b62b09d1005a524d9
Pavel Naydenov <pavel.snk@gmail.com>
2025-01-29 09:27:46 +0300

    изменения в файле 345

--- 345.py
+++ 345.py
@@ -1,9 +1,13 @@
-import re
-import sys
+from re import sub
 
-regex = r'beegeek'
-count = 0
-for elem in sys.stdin:
-    if re.search(regex, elem, re.IGNORECASE):
-        count += 1
-print(count)
+
+def func(match_obj):
+    word = match_obj.group(0)
+    temp = word[:2]
+    s = temp[::-1]
+    res = s+word[2:]
+    return res
+text = input()
+result = sub(r'\w+', func, text)
+
+print(result)
