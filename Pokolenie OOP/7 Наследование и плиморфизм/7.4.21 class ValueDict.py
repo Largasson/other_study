586d82febdfbecc9495b35c3e42bca9aaec25332
Pavel Naydenov <pavel.snk@gmail.com>
2025-04-14 15:50:40 +0300

    OOP задачки на наследование

---
+++ Pokolenie OOP/7 Наследование и плиморфизм/7.4.21 class ValueDict.py
@@ -0,0 +1,54 @@
+class ValueDict(dict):
+    def __init__(self, *args, **kwargs):
+        self.key_list = []
+        super().__init__(*args, **kwargs)
+
+
+    def key_of(self, value):
+        for k, v in self.items():
+            if v == value:
+                return k
+        return None
+
+    def keys_of(self, value):
+        for k, v in self.items():
+            if v == value:
+                self.key_list.append(k)
+        if self.key_list:
+            return self.key_list
+        return []
+
+
+
+def testing_lesson(filename: str):
+    '''
+    Функция выполняет код из каждого файла в архиве и выводит результат выполнения вашего кода и ожидаемый результат
+
+    filename - если указан с *.zip, то выполняется проверка функций из файла
+    '''
+    from zipfile import ZipFile
+    zip_name = filename if filename.endswith('zip') else filename+'.zip'
+    with ZipFile(zip_name) as z:
+        length = len(z.namelist()) // 2
+        files = [(code, out) for code, out in zip(z.namelist()[::2], z.namelist()[1::2])]
+        for file_exec, file_out in files:
+            with z.open(name=file_exec) as fi:
+                with z.open(name=file_out) as fo:
+                    code, out = fi.read().decode(), fo.read().decode()
+                    print(f'Тест № {file_exec} из {length}')
+                    print(f'\nКод: \n\n{code}\n')
+                    print('-' * 10)
+                    print('Ваш результат:')
+                    try:
+                        if filename.endswith('zip'):
+                            exec(code)
+                        else:
+                            exec(compile(filename + '(code)', 'tests', 'exec'))
+                        print(f"{'-' * 10}\nОжидаемый результат:\n{out}\n{'*' * 10}")
+                    except Exception as e:
+                        print(f'{"🚫" * 10}\n"Тест № {file_exec} завершился с `ошибкой "{type(e).__name__}: {e}\n')
+                        print(f'Ожидаемый результат: \n{out}\n{"🚫" * 50}"\n')
+
+
+
+testing_lesson('21.zip')
